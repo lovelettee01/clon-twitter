69e2ae4b1d7865a17d9779386c9f65b66221c5f8
lovelettee01 <lovelettee01@gmail.com>
2023-03-06 16:32:50 +0900

    Tweet Modify and Delete

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,13 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import { storeService } from "../firebase.config";
+import { doc, updateDoc, deleteDoc } from "firebase/firestore";
 
-const Tweet = ({ tweetInfo }) => {
-  return (
-    <div key={tweetInfo.id}>
-      <h4>{tweetInfo.comment} </h4>
-      <button>Modify</button>
-      <button>Delete</button>
-    </div>
-  );
+const COLLECTION_NAME = "tweets";
+const Tweet = ({ tweetInfo, isOwner }) => {
+    const [isEdit, setIsEdit] = useState(false);
+    const [newTweet, setNewTweet] = useState(tweetInfo.comment);
+    const onDeleteClick = async () => {
+        const ok = window.confirm("Are you Delete?");
+        if (ok) {
+            await deleteDoc(doc(storeService, COLLECTION_NAME, tweetInfo.id));
+        }
+    };
+
+    const onModifyClick = () => setIsEdit((prev) => !prev);
+    const onChange = (e) => {
+        const {
+            target: { value },
+        } = e;
+        setNewTweet(value);
+    };
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        await updateDoc(doc(storeService, COLLECTION_NAME, tweetInfo.id), {
+            comment: newTweet,
+        });
+        setIsEdit((prev) => !prev);
+    };
+    return (
+        <div key={tweetInfo.id}>
+            {isEdit ? (
+                <form onSubmit={onSubmit}>
+                    <input
+                        required
+                        placeholder="Modify Tweet Comments"
+                        type="text"
+                        value={newTweet}
+                        onChange={onChange}
+                    />
+                    <input type="submit" value="저장" />
+                    <button onClick={onModifyClick}>취소</button>
+                </form>
+            ) : (
+                <>
+                    <h4>{tweetInfo.comment} </h4>
+                    {isOwner && (
+                        <>
+                            <button onClick={onModifyClick}>Modify</button>
+                            <button onClick={onDeleteClick}>Delete</button>
+                        </>
+                    )}
+                </>
+            )}
+        </div>
+    );
 };
 
 export default Tweet;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,68 +5,75 @@ import Tweet from "components/Tweet";
 
 const COLLECTION_NAME = "tweets";
 const Home = ({ userInfo }) => {
-  const { uid } = userInfo;
-  const [tweet, setTweet] = useState("");
-  const [tweetList, setTweetList] = useState([]);
-  console.log(`Home`, tweetList);
+    const { uid } = userInfo;
+    const [tweet, setTweet] = useState("");
+    const [tweetList, setTweetList] = useState([]);
+    console.log(`Home`, tweetList);
 
-  const onSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const docRef = await addDoc(collection(storeService, COLLECTION_NAME), {
-        creatorId: uid,
-        comment: tweet,
-        createAt: Date.now(),
-      });
-      setTweet("");
-      console.log("Document written with ID: ", docRef.id);
-    } catch (e) {
-      console.error("Error adding document: ", e);
-    }
-  };
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const docRef = await addDoc(
+                collection(storeService, COLLECTION_NAME),
+                {
+                    creatorId: uid,
+                    comment: tweet,
+                    createAt: Date.now(),
+                }
+            );
+            setTweet("");
+            console.log("Document written with ID: ", docRef.id);
+        } catch (e) {
+            console.error("Error adding document: ", e);
+        }
+    };
 
-  const onChange = (e) => {
-    const {
-      target: { value },
-    } = e;
-    setTweet(value);
-  };
+    const onChange = (e) => {
+        const {
+            target: { value },
+        } = e;
+        setTweet(value);
+    };
 
-  useEffect(() => {
-    onSnapshot(collection(storeService, COLLECTION_NAME), (snapshot) => {
-      setTweetList([]);
-      console.log("Current data: ", snapshot.docs);
-      snapshot.docs.forEach((doc) => {
-        const newDoc = {
-          id: doc.id,
-          ...doc.data(),
-        };
-        setTweetList((prev) => [newDoc, ...prev]);
-      });
-    });
-  }, []);
-  return (
-    <>
-      <div>
-        <form onSubmit={onSubmit}>
-          <input
-            type={"text"}
-            placeholder={"Whas`s on your mind?"}
-            maxLength={120}
-            onChange={onChange}
-            value={tweet}
-          />
-          &nbsp;
-          <input type={"submit"} value={"tweet"} />
-        </form>
+    useEffect(() => {
+        onSnapshot(collection(storeService, COLLECTION_NAME), (snapshot) => {
+            setTweetList([]);
+            console.log("Current data: ", snapshot.docs);
+            snapshot.docs.forEach((doc) => {
+                const newDoc = {
+                    id: doc.id,
+                    ...doc.data(),
+                };
+                setTweetList((prev) => [newDoc, ...prev]);
+            });
+        });
+    }, []);
+    return (
+        <>
+            <div>
+                <form onSubmit={onSubmit}>
+                    <input
+                        type={"text"}
+                        placeholder={"Whas`s on your mind?"}
+                        maxLength={120}
+                        onChange={onChange}
+                        value={tweet}
+                    />
+                    &nbsp;
+                    <input type={"submit"} value={"tweet"} />
+                </form>
 
-        <div>
-          {tweetList.map((tweet) => (
-            <Tweet key={tweet.id} tweetInfo={tweet} />
-          ))}
-        </div>
-      </div>
-    </>
-  );
+                <div>
+                    {tweetList.map((tweet) => (
+                        <Tweet
+                            key={tweet.id}
+                            tweetInfo={tweet}
+                            isOwner={tweet.creatorId === uid}
+                        />
+                    ))}
+                </div>
+            </div>
+        </>
+    );
 };
 export default Home;
